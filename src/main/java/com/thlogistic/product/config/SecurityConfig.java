2782b933d23dec1a406700f6427db79a920b08d1
trunnghieu01 <work.trunghieu.0107@gmail.com>
2023-04-16 18:40:00 +0700

    FEAT: Enable cors

--- src/main/java/com/thlogistic/product/config/SecurityConfig.java
+++ src/main/java/com/thlogistic/product/config/SecurityConfig.java
@@ -11,6 +11,11 @@ public class SecurityConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(false);
     }
 }
